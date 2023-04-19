9518470813263ede77065fdfb43f9dbcfeb116d9
nellietan <93692985+nellietan@users.noreply.github.com>
2023-04-19 11:29:39 +0800

    Update JavaScript.js

--- filter-example/JavaScript.js
+++ filter-example/JavaScript.js
@@ -1,30 +1,15 @@
-// 获取所有筛选标签
-const filterTabs = document.querySelectorAll('.filter-tab');
-// 获取所有筛选项
-const filterItems = document.querySelectorAll('.filter-item');
-
-// 为每个筛选标签添加点击事件
-filterTabs.forEach(tab => {
+tabs.forEach(tab => {
   tab.addEventListener('click', () => {
-    // 移除所有标签的 active 类
-    filterTabs.forEach(tab => {
-      tab.classList.remove('active');
-    });
-    // 添加当前标签的 active 类
+    const tabId = tab.getAttribute('data-tab');
+    const activeTab = document.querySelector('.tab.active');
+    const activeTabContent = document.querySelector('.tab-content.active');
+
+    // Remove 'active' class from current active tab and content
+    activeTab.classList.remove('active');
+    activeTabContent.classList.remove('active');
+
+    // Add 'active' class to new tab and content
     tab.classList.add('active');
-
-    // 获取当前标签的筛选器
-    const filter = tab.getAttribute('data-filter');
-
-    // 显示或隐藏每个筛选项
-    filterItems.forEach(item => {
-      if (filter === 'all') {
-        item.style.display = 'flex';
-      } else if (item.classList.contains(filter)) {
-        item.style.display = 'flex';
-      } else {
-        item.style.display = 'none';
-      }
-    });
+    document.querySelector(`.tab-content[data-tab="${tabId}"]`).classList.add('active');
   });
 });
